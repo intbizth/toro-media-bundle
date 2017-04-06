c775c779958112c12861b7168694dbed20260924
liverbool <nukboon@gmail.com>
2017-04-06 07:55:12 +0700

    update support cmf image load lazy

--- Gedmo/References/ReferencesListener.php
+++ Gedmo/References/ReferencesListener.php
@@ -2,29 +2,95 @@
 
 namespace Toro\Bundle\MediaBundle\Gedmo\References;
 
+use Doctrine\Common\EventArgs;
 use Gedmo\References\ReferencesListener as BaseReferencesListener;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-// Fix: Circular reference detected for service
 class ReferencesListener extends BaseReferencesListener
 {
     /**
-     * @var ContainerInterface
+     * {@inheritdoc}
      */
-    private $container;
-
-    public function __construct(ContainerInterface $container)
+    protected function getNamespace()
     {
-        $this->container = $container;
-
-        parent::__construct([]);
+        return __NAMESPACE__;
     }
 
-    public function getManager($type)
+    /**
+     * {@inheritdoc}
+     */
+    public function postLoad(EventArgs $eventArgs)
     {
-        return 'entity' === strtolower($type)
-            ? $this->container->get('doctrine.orm.entity_manager')
-            : $this->container->get('doctrine_phpcr.odm.document_manager')
-        ;
+        $ea = $this->getEventAdapter($eventArgs);
+        $om = $ea->getObjectManager();
+        $object = $ea->getObject();
+        $meta = $om->getClassMetadata(get_class($object));
+        $config = $this->getConfiguration($om, $meta->name);
+
+        if (isset($config['referenceOne'])) {
+            foreach ($config['referenceOne'] as $mapping) {
+                $property = $meta->reflClass->getProperty($mapping['field']);
+                $property->setAccessible(true);
+                if (isset($mapping['identifier'])) {
+                    $referencedObjectId = $meta->getFieldValue($object, $mapping['identifier']);
+                    if (null !== $referencedObjectId) {
+                        if ($mapping['lazy']) {
+                            if (null !== $referencedObjectId) {
+                                $property->setValue($object, function () use ($ea, $mapping, $referencedObjectId) {
+                                    return $ea->getSingleReference(
+                                        $this->getManager($mapping['type']),
+                                        $mapping['class'],
+                                        $referencedObjectId
+                                    );
+                                });
+                            }
+                        } else {
+                            $property->setValue(
+                                $object,
+                                $ea->getSingleReference(
+                                    $this->getManager($mapping['type']),
+                                    $mapping['class'],
+                                    $referencedObjectId
+                                )
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        if (isset($config['referenceMany'])) {
+            foreach ($config['referenceMany'] as $mapping) {
+                $property = $meta->reflClass->getProperty($mapping['field']);
+                $property->setAccessible(true);
+                if (isset($mapping['mappedBy'])) {
+                    $id = $ea->extractIdentifier($om, $object);
+                    $manager = $this->getManager($mapping['type']);
+                    $class = $mapping['class'];
+                    $refMeta = $manager->getClassMetadata($class);
+                    $refConfig = $this->getConfiguration($manager, $refMeta->name);
+                    if (isset($refConfig['referenceOne'][$mapping['mappedBy']])) {
+                        $refMapping = $refConfig['referenceOne'][$mapping['mappedBy']];
+                        $identifier = $refMapping['identifier'];
+                        $property->setValue(
+                            $object,
+                            new LazyCollection(
+                                function () use ($id, &$manager, $class, $identifier) {
+                                    $results = $manager
+                                        ->getRepository($class)
+                                        ->findBy(array(
+                                            $identifier => $id,
+                                        ));
+
+                                    return new ArrayCollection((is_array($results) ? $results : $results->toArray()));
+                                }
+                            )
+                        );
+                    }
+                }
+            }
+        }
+
+        $this->updateManyEmbedReferences($eventArgs);
     }
 }
